112a2bf1fca561e9c1e66eacbe46b9ee7e3cd13e
Adil Farooque <Adilfarq11@gmail.com>
2024-01-18 16:08:45 +0530

    Go Crud Operations with Postgresql

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Adilfarooqu/CRUD_GO
+
+go 1.21.5
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"database/sql"
+	"fmt"
+	"os"
+	_"github.com/lib/pq"
+)
+
+
+func main() {
+	
+}
