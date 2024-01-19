c6c82f51dcb63d02bc0810c32d548c6ae4c3a5bc
Adil Farooque <Adilfarq11@gmail.com>
2024-01-19 10:22:51 +0530

    DB Connection Created

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import(
+import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
-	_"github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
 
+var id int
+var name,domain string
 
 func main() {
 	
+	
 }
+
+// db connection
+
+func connectPostgresDB() *sql.DB{
+	connectTo := "user=postgres dbname =databasename password='********' host=localhost port =5432 sslmode = disable"
+	db , err := sql.Open("postgres",connectTo)
+	if err != nil{
+		fmt.Println(err)
+	}
+	return db
+}
+
