8056bce7303d477c704492bf676fa0e37b2941f0
Adil Farooque <Adilfarq11@gmail.com>
2024-01-22 11:10:13 +0530

    Delete data from db, main function will be updated

--- main.go
+++ main.go
@@ -2,66 +2,82 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
-	"io/fs"
 	"os"
 
 	_ "github.com/lib/pq"
 )
 
 var id int
-var name,domain string
+var name, domain string
 
 func main() {
-	
-	
+	var choice int
+	db := connectPostgresDB()
+	for {
+		fmt.Println("\nChoose\n1.Insert\n2.Read\n3.Update\n4.Delete\n5.Exit")
+		fmt.Println("Enter your choice:")
+		fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			insert(db)
+		case 2:
+			Read(db)
+		case 3:
+			Update(db)
+		case 4:
+			Delete(db)
+		case 5:
+			os.Exit(0)
+		}
+	}
+
 }
 
 // db connection
-func connectPostgresDB() *sql.DB{
+func connectPostgresDB() *sql.DB {
 	connectTo := "user=postgres dbname =databasename password='********' host=localhost port =5432 sslmode = disable"
-	db , err := sql.Open("postgres",connectTo)
-	if err != nil{
+	db, err := sql.Open("postgres", connectTo)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return db
 }
 
-func insert(db *sql.DB){
+func insert(db *sql.DB) {
 	fmt.Println("Enter the id:")
 	fmt.Scan(&id)
 	fmt.Println("Enter the name:")
 	fmt.Scan(&name)
 	fmt.Println("Enter the domain:")
 	fmt.Scan(&domain)
-	insertIntoPostgres(db,id,name,domain)
+	insertIntoPostgres(db, id, name, domain)
 }
 
-func insertIntoPostgres(db *sql.DB,id int,name string,domain string){
-	_,err :=db.Exec("INSERT INTO student(id,name,domain)value($1,$2,$3)",id,name,domain)
+func insertIntoPostgres(db *sql.DB, id int, name string, domain string) {
+	_, err := db.Exec("INSERT INTO student(id,name,domain)value($1,$2,$3)", id, name, domain)
 
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
-	}else{
+	} else {
 		fmt.Println("Value Inserted!!")
 	}
 }
 
-func Read(db *sql.DB){
-	rows,err := db.Query("SELECT * FROM student")
-	if err != nil{
+func Read(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM student")
+	if err != nil {
 		fmt.Println(err)
-	}else{
+	} else {
 		fmt.Println("id name domain")
 		for rows.Next() {
-			rows.Scan(&id,&name,&domain)
-			fmt.Println("%d - %s - %s \n",id,name,domain)
+			rows.Scan(&id, &name, &domain)
+			fmt.Println("%d - %s - %s \n", id, name, domain)
 		}
 	}
 }
 
-func Update(db *sql.DB){
+func Update(db *sql.DB) {
 	fmt.Println("Enter the id:")
 	fmt.Scan(&id)
 	fmt.Println("Enter new name:")
@@ -69,10 +85,22 @@ func Update(db *sql.DB){
 	fmt.Println("Enter new domain")
 	fmt.Scan(&domain)
 
-	_,err := db.Query("UPDATE SET name=$1,domain=$2, WHERE id=$3",name,domain,id)
-	if err != nil{
+	_, err := db.Query("UPDATE SET name=$1,domain=$2, WHERE id=$3", name, domain, id)
+	if err != nil {
 		fmt.Println(err)
-	}else{
-		fmt.Println("UPdated!!!")
+	} else {
+		fmt.Println("Updated!!!")
 	}
+}
+
+func Delete(db *sql.DB) {
+	fmt.Println("Enter the id:")
+	fmt.Scan(&id)
+	_, err := db.Query("DELETE FROM student WHERE id=$1", id)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Deleted!!!")
+	}
+
 }
